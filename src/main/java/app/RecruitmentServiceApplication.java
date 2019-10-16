17db9091f472045b69c43ae90b497057faa8fcaf
made.saputra38 <made.saputra38@homecredit.co.id>
2019-10-16 16:57:16 +0700

    adding function

--- src/main/java/app/RecruitmentServiceApplication.java
+++ src/main/java/app/RecruitmentServiceApplication.java
@@ -2,9 +2,17 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RecruitmentServiceApplication {
+public class RecruitmentServiceApplication extends SpringBootServletInitializer {
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RecruitmentServiceApplication.class);
+    }
 
     public static void main(String[] args){
         SpringApplication.run(RecruitmentServiceApplication.class,args);
